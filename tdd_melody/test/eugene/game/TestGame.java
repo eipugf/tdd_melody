37a88753f5cae6c3b333d7b3931449eb8e629eea
eipugf <eipugf@gmail.com>
2016-03-27 11:47:39 +0700

    Написал тест старт игры, не компилируется

---
+++ tdd_melody/test/eugene/game/TestGame.java
@@ -0,0 +1,32 @@
+package eugene.game;
+
+import javafx.stage.Stage;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+/**
+ * тест игры
+ * @author Eugene
+ */
+public class TestGame {
+    
+    public TestGame(){}
+    
+    @BeforeClass
+    public void setUp(){
+    
+    }
+    
+    @Test
+    public void testStarGame(){
+        Game game = new Game();
+        game.start();
+        assertEquals(game.isStarted()==true);
+    }
+    
+}
---
+++ tdd_melody/test/tdd_melody/MainStartNGTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tdd_melody;
+
+import javafx.stage.Stage;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Eugene
+ */
+public class MainStartNGTest {
+    
+    public MainStartNGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+
+    /**
+     * Тест метод start, класса MainStart.
+     */
+    @Test
+    public void testStart() {
+        System.out.println("start");
+        Stage primaryStage = null;
+        MainStart instance = new MainStart();
+        instance.start(primaryStage);
+        // Просмотр списка задач TODO для сгенерированного кода теста и удаление вызова по умолчанию для случаев сбоя.
+        fail("\u042d\u0442\u043e\u0442 \u0442\u0435\u0441\u0442 \u044f\u0432\u043b\u044f\u0435\u0442\u0441\u044f \u043f\u0440\u043e\u0442\u043e\u0442\u0438\u043f\u043e\u043c.");
+    }
+
+    /**
+     * Тест метод main, класса MainStart.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        MainStart.main(args);
+        // Просмотр списка задач TODO для сгенерированного кода теста и удаление вызова по умолчанию для случаев сбоя.
+        fail("\u042d\u0442\u043e\u0442 \u0442\u0435\u0441\u0442 \u044f\u0432\u043b\u044f\u0435\u0442\u0441\u044f \u043f\u0440\u043e\u0442\u043e\u0442\u0438\u043f\u043e\u043c.");
+    }
+    
+}
